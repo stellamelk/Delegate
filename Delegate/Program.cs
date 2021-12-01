63c13fe4c3d9a8c8d311e25fef1681b92bccda23
Stella Melkonyan <stella.melkonyan.benivo@outlook.com>
2021-12-02 02:01:07 +0400

    Add project files.

---
+++ Delegate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    class Program
+    {
+        delegate void Message(); // 1. Delegate declaration
+        static void Main(string[] args)
+        {
+            Message mes; // 2. Create a delegate variable
+            if (DateTime.Now.Hour < 12)
+            {
+                mes = GoodMorning; // 3. Assign the address of the method to this variable
+            }
+            else
+            {
+                mes = GoodEvening;
+            }
+            mes();// 4. Call the method
+        }
+        private static void GoodMorning()
+        {
+            Console.WriteLine("Good Morning");
+        }
+        private static void GoodEvening()
+        {
+            Console.WriteLine("Good Evening");
+        }
+    }
+}
---
+++ Delegate1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate1
+{
+    class Program
+    {
+        delegate int Operation(int x, int y);
+        static void Main(string[] args)
+        {
+            //Assignment of method address via constructor
+            Operation del = Add;// Delegate points to the Add method
+            Operation del1 = new Operation(Add); //Method reference assignment
+            int result = del(4,5);// actually Add(4,5)
+            Console.WriteLine(result);
+
+            Math math = new Math();
+            Operation del2 = math.Sum;
+            int result1 = del2(2, 3);// math.Sum(2,3)
+            Console.WriteLine(result1); // 5 
+
+            Console.Read();
+        }
+        private static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        private static int Multyply(int x, int y)
+        {
+            return x * y;
+        }
+    }
+    class Math
+    {
+        public int Sum(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
